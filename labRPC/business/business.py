3c22e9c77e4717037b8bb4a07d1a26110c6a834d
João Vitor Lehmen Sanmartin <jvsanmartin@yahoo.com.br>
2024-04-12 14:59:06 -0300

    Add files via upload

---
+++ labRPC/business/business.py
@@ -0,0 +1,23 @@
+# Classe business.py
+
+import uuid
+
+class Produto:
+    def __init__(self, id=None, nome=None, descricao=None, preco=None, quantidade=None, fabricante_id=None, categoria_id=None):
+        self.id = id if id else uuid.uuid4()
+        self.nome = nome
+        self.descricao = descricao
+        self.preco = preco
+        self.quantidade = quantidade
+        self.fabricante_id = fabricante_id
+        self.categoria_id = categoria_id
+
+class Categoria:
+    def __init__(self, id=None, nome=None):
+        self.id = id if id else uuid.uuid4()
+        self.nome = nome
+
+class Fabricante:
+    def __init__(self, id=None, nome=None):
+        self.id = id if id else uuid.uuid4()
+        self.nome = nome
